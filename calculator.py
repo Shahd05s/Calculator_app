385ea533f1be263b22cd5e44c75fd5e9ac56212b
Sultan-Alobaid <sultanalobaid6@gmail.com>
2025-02-11 14:37:56 +0000

    i made the functions

---
+++ calculator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 12:04:20 2025
+
+@author: sultanalobaid
+"""
+#import math
+import math
+#creat a class for all the formulas
+class mathmatics:
+    def __init__(self,addition,subtraction,multiplication,division,sine,cosine,tangent,arcsine,arccos,arctan,square,square_root,pi):
+#define functions for all the math formulas
+        def addition (self, a,b):
+            return a+b
+        def subtraction (self, a,b):
+            return a-b
+        def  multiplication (self, a,b):
+            return a*b
+        def divsion (self, a,b):
+            return a/b
+        def sine (self, a):
+            return math.sin(a)
+        def cosine (self, a):
+            return math.cos(a)
+        def tangent (self, a):
+            return math.tan(a)
+        def arcsine(self, a):
+            return math.asin(a)
+        def arccos (self, a):
+            return math.acos(a)
+        def arctan(self, a):
+            return math.atan(a)
+        def square (self, a):
+            return a**2
+        def square_root(self, a):
+            return math.sqrt(a)
+        def pi (self):
+            return math.pi
+        
